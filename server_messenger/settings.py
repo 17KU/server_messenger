5b3b0ba8b090ab33f6898ef3b5f255f8f8122dc3
YoumHyeJi <yheji0321@gmail.com>
2021-11-15 17:35:58 +0900

    디비 내용 수정

--- server_messenger/settings.py
+++ server_messenger/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'server_messenger.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '203.252.166.201',
+        'NAME': 'server_sns',
+        'USER': 'root',
+        'PASSWORD': 'admin123!',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
